38ea46241737814f6f3955442c77262213ae43b5
Kat Henson <kathenson2@gmail.com>
2023-02-24 14:52:25 -0700

    fix code for testing empty arrays using if else statement

--- js/solution004.js
+++ js/solution004.js
@@ -3,10 +3,11 @@
 Note: Empty arrays should return 0. */
 
 function findAverage(array) {
-    return array.reduce((sum, current) => sum + current) / array.length 
-    if (array.length === 0) {
-    return 0;
-    }
+  if (array.length === 0) {
+  return 0;
+  } else {
+  return array.reduce((sum, current) => sum + current) / array.length 
   }
+}
 
 console.log(findAverage([1,2,3,4]))
